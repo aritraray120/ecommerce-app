e074257327a85f9de6fb589e6ff457ac3d5a3eb2
Aritra Ray <aritraray120@gmail.com>
2024-11-09 22:18:51 +0530

    Initial commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+
+function App() {
+  return ( 
+  
+  <div className="px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]">
+
+
+    <Routes>
+
+    </Routes>
+
+  </div> 
+  
+);
+}
+
+export default App;
