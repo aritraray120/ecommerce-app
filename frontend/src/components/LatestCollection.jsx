122981f5d8182022e13d4fe9af0a5134cb3d6b1d
Aritra Ray <aritraray120@gmail.com>
2025-01-01 20:30:46 +0530

    u

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -1,7 +1,20 @@
+import { useContext } from "react";
+import { ShopContext } from "../context/ShopContext";
+import Title from "./Title";
+
 function LatestCollection() {
+
+    const { products } = useContext(ShopContext);
+
     return ( 
 
-        <div></div>
+        <div className="my-10">
+
+            <div className="text-center py-8 text-3xl">
+                <Title text1={'LATEST'} text2={'COLLECTIONS'} />
+            </div>
+
+        </div>
 
      );
 }
